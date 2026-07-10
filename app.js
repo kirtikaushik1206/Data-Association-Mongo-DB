99abccc4717911683ad915f548ca8aae53e8c526
Kirti Kaushik <kirtikaushik1206@gmail.com>
2026-07-10 16:03:04 +0530

    Created User

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const express= require('express');
 const app = express();
-const userModel =('./models/user');
-const postModel =('./models/post');
+const userModel = require('./models/user');
+const postModel = require('./models/post');
+
+app.get('/create' , async function (req,res) {
+    let user= await userModel.create({
+        username:"kirti",
+        age:19,
+        email:"kirts@gmail.com"
+    });
+    res.send(user);
+})
+
+app.listen(3000);
