22aa341a26cb28352794a0cbc30d7759811c6130
Kirti Kaushik <kirtikaushik1206@gmail.com>
2026-07-10 17:43:15 +0530

    Created Post

--- app.js
+++ app.js
@@ -11,5 +11,15 @@ app.get('/create' , async function (req,res) {
     });
     res.send(user);
 })
+app.get('/post' , async function(req,res){
+    let post = await postModel.create({
+        postdata: "hi lol",
+        user:"6a50dfed195f1e5bac6133e6"
+    })
+    let user = await userModel.findOne({_id:"6a50dfed195f1e5bac6133e6"});
+    user.posts.push(post);
+    await user.save();
+    res.send(post, user);
+})
 
 app.listen(3000);
